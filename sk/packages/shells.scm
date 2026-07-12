5b5b69e6544e3e0506837efbce218651c13b2c41
Rafael Oliveira <r0liveira@icloud.com>
2026-07-12 05:03:52 -0400

    Fix fish latest test phase

--- sk/packages/shells.scm
+++ sk/packages/shells.scm
@@ -1,7 +1,9 @@
 (define-module (sk packages shells)
   #:use-module (gnu packages shells)
   #:use-module (guix download)
-  #:use-module (guix packages))
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (guix utils))
 
 (define-public fish-latest
   (package
@@ -15,4 +17,49 @@
                            "releases/download/" version "/"
                            "fish-" version ".tar.xz"))
        (sha256
-        (base32 "01z7cfxch2n7mkm3c065pz19ikwalip1za2gx3h5799gvz3n5brk"))))))
+        (base32 "01z7cfxch2n7mkm3c065pz19ikwalip1za2gx3h5799gvz3n5brk"))))
+    (arguments
+     (substitute-keyword-arguments (package-arguments fish)
+       ((#:phases phases)
+        #~(modify-phases #$phases
+            (replace 'patch-tests
+              (lambda* (#:key inputs native-inputs #:allow-other-keys)
+                (define (delete-file-if-exists file)
+                  (when (file-exists? file)
+                    (delete-file file)))
+
+                (let* ((coreutils
+                        (dirname
+                         (dirname
+                          (search-input-file
+                           (or native-inputs inputs) "bin/pwd"))))
+                       (bash
+                        (dirname
+                         (dirname
+                          (search-input-file
+                           (or native-inputs inputs) "bin/bash")))))
+                  ;; These tests either fail in Guix build containers or assume
+                  ;; global filesystem paths. Keep this aligned with Guix's fish
+                  ;; package, but tolerate tests that disappeared upstream.
+                  (for-each delete-file-if-exists
+                            '("tests/checks/jobs.fish"
+                              "tests/checks/noshebang.fish"
+                              "tests/checks/__fish_posix_shell.fish"
+                              "tests/checks/__fish_migrate.fish"))
+                  (substitute* "tests/checks/vars_as_commands.fish"
+                    (("/usr/bin") "/tmp"))
+                  (substitute* "tests/checks/cd.fish"
+                    (("cd bin") "cd tmp"))
+                  (substitute* (cons* "src/builtins/test.rs"
+                                      "src/highlight/file_tester.rs"
+                                      "src/highlight/highlight.rs"
+                                      (find-files "tests"))
+                    (("/bin/sh" sh) (string-append bash sh))
+                    (("/usr/bin/en\"") (string-append coreutils "/bin/en\""))
+                    (("/usr/bin/e\"") (string-append coreutils "/bin/e\""))
+                    (("\"/bin") "\"/tmp")
+                    (("\"/usr") "\"/tmp"))
+                  (substitute* "tests/checks/colon-delimited-var.fish"
+                    (("/usr/bin:a:.:b") "/tmp/bin:a:.:b"))
+                  (substitute* "tests/test_driver.py"
+                    (("\"cc\"") "\"gcc\""))))))))))
